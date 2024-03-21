8429135241df7e199cc5b42327860a3a03e52a3c
Bagus Aditama <bagusaditama.pp@gmail.com>
2024-03-21 22:04:58 +0700

    feat: counter app with bloc

---
+++ lib/counter_bloc/counter_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_bloc_firebase/counter_bloc/counter_event.dart';
+import 'package:learn_bloc_firebase/counter_bloc/counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc():super(CounterInitialState()) {
+    on<CounterIncrementEvent>((event, emit) {
+      emit(CounterIncrementState(state.counter + 1));
+    });
+
+    on<CounterDecrementEvent>((event, emit) {
+      emit(CounterDecrementState(state.counter - 1));
+    });
+  }
+}
